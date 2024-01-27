5be6054c24c0bde6ea6c2a0a3a082103eb84588a
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-01-27 12:31:57 +0000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-evenly bg-[#0F19A0] h-[90px] items-center">
+        <h1 className="text-[#ffffff] text-5xl font-bold">
+          Property Ladder 🪜
+        </h1>
+      </div>
+      <div className="flex justify-evenly bg-[#8d8d92] h-[4px] items-center"></div>
+    </>
+  );
+};
