a1bfe3b2d7e75fad0810cfd2a5fb99074d268e6e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-01-28 08:23:18 +0000

    changed navbar

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,8 @@
 export const Description = () => {
   return (
     <>
-      <div className="flex justify-evenly h-[330px]">
+      <div className="flex justify-evenly h-[250px]">
         <div className="flex flex-col justify-evenly w-1/2">
-          <h2 className="text-blue-900 text-5xl font-bold">
-            Stamp Duty Calculator
-          </h2>
           <div>
             <div className="border-l-[7px] border-[#6FD8D8] h-[40px] pl-3  text-[19px] mb-10">
               Calculate the Stamp Duty you will owe if you&apos;re purchasing a
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     <>
       <div className="flex justify-evenly bg-[#0F19A0] h-[125px] items-center">
         <h1 className="text-[#ffffff] text-6xl font-bold">
-          Property Ladder 🪜
+          Stamp Duty Calculator
         </h1>
       </div>
       <div className="flex justify-evenly bg-[#b8b8c2] h-[3px] items-center"></div>
