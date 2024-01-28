f4b8f1c312d8afbc293837748e676c3bc99253e5
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-01-28 09:52:32 +0000

    added logo to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 export const Navbar = () => {
   return (
     <>
-      <div className="flex justify-evenly bg-[#0F19A0] h-[125px] items-center">
-        <h1 className="text-[#ffffff] text-6xl font-bold">
+      <div className="flex justify-around bg-[#0F19A0] h-[125px] items-center">
+        <img
+          className="w-[125px] h-[100px] rounded-md"
+          src="./public/stamp-fav.png"
+          alt="stamp duty logo"
+        />
+        <h1 className="text-[#ffffff] text-7xl font-bold">
           Stamp Duty Calculator
         </h1>
+        <img
+          className="w-[125px] h-[100px] rounded-md"
+          src="./public/stamp-fav.png"
+          alt="stamp duty logo"
+        />
       </div>
       <div className="flex justify-evenly bg-[#b8b8c2] h-[3px] items-center"></div>
     </>
