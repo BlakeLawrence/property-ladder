2504e0bcd43654bdde87dc31db2d463c4e0041ad
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-01-29 08:31:21 +0000

    navbar and description responsive complete

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,14 +1,14 @@
 export const Description = () => {
   return (
     <>
-      <div className="flex justify-evenly h-[150px] md:h-[250px]">
-        <div className="flex sm:flex-col sm:justify-evenly sm:w-1/2">
-          <div className="flex sm:flex-col p-5 sm:p-0">
-            <div className="border-l-[7px] border-[#6FD8D8] h-[40px] pl-3 text-[10px] md:text-[19px] mb-10">
+      <div className="flex justify-evenly h-[125px] md:h-[250px]">
+        <div className="flex sm:flex-col md:justify-evenly md:w-1/2">
+          <div className="flex md:flex-col pt-5 pb-5 pl-2 pr-1 lg:p-0">
+            <div className="border-l-[7px] border-[#6FD8D8] h-[40px] pl-3 text-[10px] sm:text-[11px] md:text-[15px] lg:text-[19px] mb-10">
               Calculate the Stamp Duty you will owe if you&apos;re purchasing a
               residential property in England or Scotland.
             </div>
-            <p className="text-[9px] md:text-[17px]">
+            <p className="text-[9px] sm:-text[10px] md:text-[14px] lg:text-[17px] pl-2 pr-5 sm:pl-0">
               Our calculator lets you know the amount of Stamp Duty you&apos;ll
               be liable to pay. It&apos;ll work out how much you&apos;ll owe,
               whether you&apos;re a first-time buyer, moving home, or buying an
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <>
-      <div className="flex justify-around bg-[#0F19A0]  h-[125px] items-center">
+      <div className="flex justify-around bg-[#0F19A0] h-[125px] items-center">
         <img
           className="w-[40px] h-[40px] md:w-[80px] md:h-[80px] lg:w-[125px] lg:h-[100px] rounded-md"
           src="./stamp-fav.png"
@@ -16,7 +16,7 @@ export const Navbar = () => {
           alt="stamp duty logo"
         />
       </div>
-      <div className="flex justify-evenly bg-[#b8b8c2] h-[3px] items-center"></div>
+      <div className="flex bg-[#b8b8c2] h-[3px]"></div>
     </>
   );
 };
