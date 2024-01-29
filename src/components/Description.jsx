52fcb6fe2a44b3fa0024a63b1ec62b2b4ceb285d
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-01-29 08:02:49 +0000

    starting responsiveness

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,14 +1,14 @@
 export const Description = () => {
   return (
     <>
-      <div className="flex justify-evenly h-[250px]">
+      <div className="flex justify-evenly h-[150px] md:h-[250px]">
         <div className="flex flex-col justify-evenly w-1/2">
           <div>
-            <div className="border-l-[7px] border-[#6FD8D8] h-[40px] pl-3  text-[19px] mb-10">
+            <div className="border-l-[7px] border-[#6FD8D8] h-[40px] pl-3   md:text-[19px] mb-10">
               Calculate the Stamp Duty you will owe if you&apos;re purchasing a
               residential property in England or Scotland.
             </div>
-            <p className="text-[17px]">
+            <p className="md:text-[17px]">
               Our calculator lets you know the amount of Stamp Duty you&apos;ll
               be liable to pay. It&apos;ll work out how much you&apos;ll owe,
               whether you&apos;re a first-time buyer, moving home, or buying an
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 export const Navbar = () => {
   return (
     <>
-      <div className="flex justify-around bg-[#0F19A0] h-[125px] items-center">
+      <div className="flex justify-around bg-[#0F19A0]  h-[125px] items-center">
         <img
-          className="w-[125px] h-[100px] rounded-md"
+          className="w-[40px] h-[40px] md:w-[125px] md:h-[100px] rounded-md"
           src="./stamp-fav.png"
           alt="stamp duty logo"
         />
-        <h1 className="text-[#ffffff] text-6xl font-bold">
+        <h1 className="text-[#ffffff] text-lg md:text-6xl font-bold">
           Stamp Duty Calculator
         </h1>
         <img
-          className="w-[125px] h-[100px] rounded-md"
+          className="w-[40px] h-[40px] md:w-[125px] md:h-[100px] rounded-md"
           src="./stamp-fav.png"
           alt="stamp duty logo"
         />
